9f95d3768ecc5b0a51a662ddd4094ef5c7bcb4c8
Pierrard <t.pierrard.131.198@outlook.fr>
2026-02-24 18:10:28 +0100

    Force HTTPS in production

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -5,7 +5,7 @@ namespace App\Providers;
 use Illuminate\Support\Facades\View;
 use Illuminate\Support\ServiceProvider;
 use App\View\Composers\AdminNotificationComposer;
-
+use Illuminate\Support\Facades\URL;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -20,9 +20,6 @@ class AppServiceProvider extends ServiceProvider
     /**
      * Bootstrap any application services.
      */
-    public function boot(): void
-    {
-        View::composer('layouts.navigation', AdminNotificationComposer::class);
-
-    }
+    public function boot(): void { View::composer('layouts.navigation', AdminNotificationComposer::class); // Force HTTPS en production if (config('app.env') === 'production') { URL::forceScheme('https'); } }
+}
 }
