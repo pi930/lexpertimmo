f46c7be7bc68c6139a0ac0487707ade87b215a68
Thomas <pi930@users.noreply.github.com>
2026-07-30 23:06:32 +0200

    Fix Vite base path for Render HTTPS

--- vite.config.js
+++ vite.config.js
@@ -2,12 +2,14 @@ import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 
 export default defineConfig({
-    plugins: [laravel({
-        input: ['resources/css/app.css', 'resources/js/app.js'],
-        refresh: true,
-    })],
+    base: '',
+    plugins: [
+        laravel({
+            input: ['resources/css/app.css', 'resources/js/app.js'],
+            refresh: true,
+        }),
+    ],
     build: {
         outDir: 'public/build',
     },
 });
-
